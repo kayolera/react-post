165add7e0d4c3777f395ac616ba97b50f40795f3
kayolera Obadia <kayoobadia@gmail.com>
2022-02-01 04:55:43 +0200

    states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+class App extends Component{
+
+  state = {
+    name: '',
+    counter: 0
+  }
+
+  handlePclick = () => {
+    this.setState({name:"kayolera clicou"})
+  }
+
+  handleAclick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter+1})
+  }
+
+  render(){
+    const {name, counter} = this.state;
+    return (
+     
+      <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <p onClick={this.handlePclick}>
+           Edit <code>src/App.js</code> and save to reload.
+           name: {name}
+           <br/>
+           counter: {counter}
+         </p>
+         <a
+         onClick={this.handleAclick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -19,7 +43,30 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
