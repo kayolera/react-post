ab0a518a17437e08388294a31f8de3450e0bcc6a
kayolera Obadia <kayoobadia@gmail.com>
2022-02-03 13:25:41 +0200

    sem placeholder

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -2,7 +2,7 @@
 import "./styles.css"
 export const TextInput = ({searchvalue,handleChage}) =>{
     return (
-        <input className="text-input" type="search" onChange={handleChage} value={searchvalue} placeholder="search..."/>
+        <input className="text-input" type="search" onChange={handleChage} value={searchvalue} placeholder=""/>
     )
 
 }
